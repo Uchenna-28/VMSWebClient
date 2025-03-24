44707b3c63cad8a2425c02e572c46463fc2e14e1
uchenna28 <uchennaonuoha15@gmail.com>
2025-03-24 10:58:57 +0100

    updated web integration

--- src/app/employee/employee-calender/employee-calender.component.ts
+++ src/app/employee/employee-calender/employee-calender.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   ReactiveFormsModule,
@@ -32,36 +32,48 @@ import { MeetingButtonComponent } from '../../shared/components/meeting-button/m
 })
 export class EmployeeCalenderComponent {
   isAddMeetingModalOpen = false;
-  meetingRoomsOptions = [{ label: 'Board Room', value: 'Board Room' }];
-  scheduleMeetingForm = new FormGroup({
-    title: new FormControl('', Validators.required),
-    agenda: new FormControl('', Validators.required),
-    startDate: new FormControl('', Validators.required),
-    startTime: new FormControl('', Validators.required),
-    endTime: new FormControl('', Validators.required),
-    meetingRoom: new FormControl('', Validators.required),
-    participants: new FormControl([], Validators.required),
-  });
-  onButtonClick() {
-    this.scheduleMeetingForm.reset();
-    this.isAddMeetingModalOpen = true;
-  }
-  onScheduleMeeting() {
-    this.scheduleMeetingForm.reset();
-    this.isAddMeetingModalOpen = true;
-  }
-  closeAddMeetingModal() {
-    this.isAddMeetingModalOpen = false;
-  }
-  submitMeeting() {
-    if (this.scheduleMeetingForm.valid) {
-      const newMeeting = this.scheduleMeetingForm.value;
-      console.log('New Meeting:', newMeeting);
-
-      // Optional: Add to tableData here
-      // this.tableData.push(newMeeting);
-
-      this.closeAddMeetingModal();
+    meetingRoomsOptions = [{ label: 'Board Room', value: 'Board Room' }];
+    today: Date = new Date(); // Store today's date
+  
+    @ViewChild('mainCalendar') mainCalendar!: FullCalenderComponent;
+  
+    scheduleMeetingForm = new FormGroup({
+      title: new FormControl('', Validators.required),
+      agenda: new FormControl('', Validators.required),
+      startDate: new FormControl('', Validators.required),
+      startTime: new FormControl('', Validators.required),
+      endTime: new FormControl('', Validators.required),
+      meetingRoom: new FormControl('', Validators.required),
+      participants: new FormControl([], Validators.required),
+    });
+  
+    ngAfterViewInit() {
+      if (this.mainCalendar) {
+        // Ensure the second calendar starts in "Week View" at today's date
+        this.mainCalendar.changeView('timeGridWeek', this.today);
+      }
     }
-  }
+  
+    onDateSelected(event: any) {
+      console.log('Selected Date:', event.date);
+      if (this.mainCalendar) {
+        this.mainCalendar.changeView('timeGridWeek', event.date); // Update second calendar
+      }
+    }
+    onButtonClick() {
+      this.scheduleMeetingForm.reset();
+      this.isAddMeetingModalOpen = true;
+    }
+  
+    closeAddMeetingModal() {
+      this.isAddMeetingModalOpen = false;
+    }
+  
+    submitMeeting() {
+      if (this.scheduleMeetingForm.valid) {
+        const newMeeting = this.scheduleMeetingForm.value;
+        console.log('New Meeting:', newMeeting);
+        this.closeAddMeetingModal();
+      }
+    }
 }
--- src/app/shared/components/full-calender/full-calender.component.ts
+++ src/app/shared/components/full-calender/full-calender.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FullCalendarModule } from '@fullcalendar/angular';
+import { Component, Input, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { FullCalendarModule, FullCalendarComponent } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
@@ -9,16 +8,17 @@ import { CalendarOptions } from '@fullcalendar/core';
 @Component({
   selector: 'app-full-calender',
   standalone: true,
-  imports: [CommonModule, FullCalendarModule],
+  imports: [FullCalendarModule],
   templateUrl: './full-calender.component.html',
   styleUrl: './full-calender.component.css'
 })
 export class FullCalenderComponent implements OnInit {
   @Input() events: any[] = [];
-  @Input() allowedViews: string[] = ['dayGridMonth', 'timeGridWeek', 'timeGridDay']; // Default views
+  @Input() allowedViews: string[] = ['dayGridMonth', 'timeGridWeek', 'timeGridDay']; 
   @Input() initialView: string = 'dayGridMonth';
   @Input() editable: boolean = true;
   @Input() selectable: boolean = true;
+  @Input() startDate: Date = new Date(); // Ensure it starts with today's date
   @Input() showToolbar: boolean = true;
   @Input() buttonText: any = {
     prev: '‹',
@@ -28,7 +28,11 @@ export class FullCalenderComponent implements OnInit {
     week: 'Week',
     day: 'Day'
   };
-  @Input() headerToolbar: any = null; // Allow full customization of toolbar
+  @Input() headerToolbar: any = null;
+
+  @Output() dateClick = new EventEmitter<any>();
+
+  @ViewChild(FullCalendarComponent) calendar!: FullCalendarComponent;
 
   calendarOptions!: CalendarOptions;
 
@@ -40,11 +44,24 @@ export class FullCalenderComponent implements OnInit {
       editable: this.editable,
       selectable: this.selectable,
       buttonText: this.buttonText,
+      initialDate: this.startDate, // Start with today's date
       headerToolbar: this.headerToolbar !== null
-        ? this.headerToolbar // Use the provided toolbar configuration
+        ? this.headerToolbar 
         : this.showToolbar
-        ? { left: this.allowedViews.join(','), center: 'title', right: 'prev,next' } // Default toolbar
-        : false, // Hide toolbar if showToolbar is false
+        ? { left: this.allowedViews.join(','), center: 'title', right: 'prev,next' }
+        : false,
+      dateClick: this.handleDateClick.bind(this) 
     };
+  }
+
+  handleDateClick(arg: any) {
+    console.log('Date clicked:', arg.date);
+    this.dateClick.emit(arg);
+  }
+
+  changeView(viewType: string, date: Date) {
+    if (this.calendar) {
+      this.calendar.getApi().changeView(viewType, date);
+    }
   }
 }
