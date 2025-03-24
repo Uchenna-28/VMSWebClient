b4c8953e65bd6e87b4138db709c08f0d6008ff00
Michael Agbogidi <michael.agbogidi@verraki.com>
2025-03-24 12:53:40 +0000

    Deleted src

--- src/app/core/core.providers.ts
+++
@@ -1,7 +0,0 @@
-import { provideHttpClient } from '@angular/common/http';
-import { AuthService } from './services/auth.service';
-
-export const CORE_PROVIDERS = [
-  provideHttpClient(), // ✅ Correct: No need for importProvidersFrom()
-  AuthService
-];
